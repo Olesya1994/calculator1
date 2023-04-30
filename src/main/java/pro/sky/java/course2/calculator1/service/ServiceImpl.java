b951df0cd49329d9afa170400df85756b5973c16
Olesya1994 <olesia.liakhor@gmail.com>
2023-04-30 16:25:27 +0300

    2st commit

--- src/main/java/pro/sky/java/course2/calculator1/service/ServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator1/service/ServiceImpl.java
@@ -2,10 +2,7 @@ package pro.sky.java.course2.calculator1.service;
 
 @org.springframework.stereotype.Service
 public class ServiceImpl implements Service {
-    private int Result;
-    private int num1;
-   private int num2;
-    @Override
+        @Override
     public String getResultMinus(int num1, int num2){
        return num1 + " - "+ num2 +" = "+ (num1-num2);
     }
@@ -21,5 +18,4 @@ public class ServiceImpl implements Service {
     public String getResultDivide(int num1, int num2){
         return num1 + " / "+ num2 +" = "+ (num1/num2);
     }
-
 }
